365b68a14dc45bac5a9ca2fd39d9e43e31405d03
Daniel Eldjarn <danieleldjarn@gmail.com>
2013-10-30 22:49:27 +0000

    First version of twitter app

--- app.rb
+++ app.rb
@@ -2,3 +2,20 @@
 
 get '/' do erb :index
 end
+
+get '/follows' do 
+  @user1 = params[:user1]
+  @user2 = params[:user2] # Not implemented yet!
+  @following = is_following?(@user1, @user2) erb :follows
+end
+
+def twitter_id(screen_name)
+  Twitter.user(screen_name).id
+end
+
+def is_following?(a, b)
+  followers = Twitter.follower_ids(twitter_id(b)).ids 
+  followers.include?(twitter_id(a))
+end
+
+
